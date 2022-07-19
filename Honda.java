46765d177cba3bd25b90812e75dda581d7cb8701
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-19 12:50:06 +0530

    Add files via upload

---
+++ Honda.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Honda extends Cars{
+public void seat() {
+	System.out.println("Soft beded seat");
+}
+public void wireless() {
+	System.out.println("Bluetooth connectivity");
+}
+}
