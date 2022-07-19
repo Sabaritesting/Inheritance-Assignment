b77c0bef17ec6070b8201d28d4b447e96974c8f9
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-19 12:49:21 +0530

    Add files via upload

---
+++ Audi.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Audi extends Cars {
+public void sensor() {
+	System.out.println("Tracking Sensor");
+}
+public void camera() {
+System.out.println("Reverse parking camera");	
+}
+}
