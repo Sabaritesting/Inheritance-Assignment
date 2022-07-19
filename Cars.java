fd2332e24b2b1d2c82e8fb1fdcdbce65ccde82b8
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-19 12:48:57 +0530

    Add files via upload

---
+++ Cars.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Cars extends fourwheeler {
+	
+public void engine(){
+	System.out.println("Engine Model:");
+}
+public void rpm() {
+System.out.println("RPM Capacity");	
+}
+}
