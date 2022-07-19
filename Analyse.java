6d21ab06ecc3770dc9bf9a610481b455c8549033
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-19 12:49:41 +0530

    Add files via upload

---
+++ Analyse.java
@@ -0,0 +1,23 @@
+package Inheritance;
+
+public class Analyse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Audi a=new Audi();
+a.camera();
+a.sensor();
+a.engine();
+a.rpm();
+a.cars();
+
+Honda s=new Honda();
+s.seat();
+s.wireless();
+s.engine();
+s.rpm();
+s.cars();
+
+	}
+
+}
